4727e9de82b928f89443c926c87d5e2123998ec4
Facundo Javier Gelatti <javiergelatti@gmail.com>
2014-11-15 15:16:06 -0300

    Agregar operaciones con numero complejos

--- js/dominio/NumeroComplejo.js
+++ js/dominio/NumeroComplejo.js
@@ -1,5 +1,30 @@
 var dominio = (function(dominio) {
 
+    var e = Math.E;
+    function cos(x) {
+        return Math.cos(x);
+    }
+    function sen(x) {
+        return Math.sin(x);
+    }
+    function sqrt(x) {
+        return Math.sqrt(x);
+    }
+    function pow(y, x) {
+        return Math.pow(y, x);
+    }
+    function exp(x) {
+        return pow(e, x);
+    }
+    function cosh(x) {
+        var ex = exp(x);
+        return 1/2 * (1/ex + ex);
+    }
+    function senh(x) {
+        var ex = exp(x);
+        return 1/2 * (-1/ex + ex);
+    }
+
     dominio.NumeroComplejo = function(a, b) {
         this._a = a;
         this._b = b;
@@ -23,7 +48,7 @@ var dominio = (function(dominio) {
         },
         mod : {
             get : function() {
-                return Math.sqrt(this.mod2);
+                return sqrt(this.mod2);
             }
         },
         sumar : {
@@ -47,8 +72,21 @@ var dominio = (function(dominio) {
         dividir : {
             value : function(c2) {
                 var c1 = this;
-                var sc = Math.pow(c2.re, 2) + Math.pow(c2.im, 2);
+                var sc = pow(c2.re, 2) + pow(c2.im, 2);
                 return new dominio.NumeroComplejo((c1.re*c2.re + c1.im*c2.im)/sc, (c1.im*c2.re - c1.re*c2.im)/sc);
+            }
+        },
+        sen : {
+            value : function() {
+                var z = this;
+                return new dominio.NumeroComplejo(cosh(z.im) * sen(z.re), cos(z.re) * senh(z.im));
+            }
+        },
+        exp : {
+            value : function() {
+                var z = this;
+                var ea = exp(z.re);
+                return new dominio.NumeroComplejo(ea * cos(z.im), ea * sen(z.im));
             }
         }
     });
